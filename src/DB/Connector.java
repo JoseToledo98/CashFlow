8abc25fa11b5ee03d2d2fac8ca3576483ba1ccc6
José Miguel <miguepumas2010@gmail.com>
2021-04-05 19:57:20 -0500

    Adapter conexion

--- src/DB/Connector.java
+++ src/DB/Connector.java
@@ -1,5 +1,27 @@
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connector {
+	
+public Connection getConnectionMySql() {
+		//modifiquen la conexion con sus datos
+		Connection connection = null;
+		String url = "jdbc:mysql://localhost:3306/cashflow?zeroDateTimeBehavior=convertToNull&serverTimezone=UTC";
+		String user = "root";
+		String password = "11minutos";
+		
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println(connection);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+		}
 
 }
+
